e259279167b48692c9276323df68a4bc74ed2db6
cilikinari <cilikinari2005@gmail.com>
2026-04-12 12:03:50 +0800

    feat: Add AppIcons class for commonly used icons in the application

---
+++ lib/icons/app_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const IconData search = Icons.search;
+  static const IconData location = Icons.location_on;
+  static const IconData clear = Icons.clear;
+  static const IconData check = Icons.check;
+}
