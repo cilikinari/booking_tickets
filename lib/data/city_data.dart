08970ad29ef4a3b3a201b73bf0bbba5b49cd4ecc
cilikinari <cilikinari2005@gmail.com>
2026-04-12 12:03:43 +0800

    feat: Add CityData class with a list of cities for booking functionality

---
+++ lib/data/city_data.dart
@@ -0,0 +1,19 @@
+class CityData {
+  static const List<String> cities = [
+    "Ambon",
+    "Balikpapan",
+    "Bandung",
+    "Banjarmasin",
+    "Batam",
+    "Bekasi",
+    "Bogor",
+    "Denpasar",
+    "Jakarta",
+    "Makassar",
+    "Medan",
+    "Semarang",
+    "Surabaya",
+    "Tangerang",
+    "Yogyakarta",
+  ];
+}
