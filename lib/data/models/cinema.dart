e77c3b39a681269229162217a210d4eac932381e
cilikinari <cilikinari2005@gmail.com>
2026-06-05 21:53:23 +0800

    commit

--- lib/data/models/cinema.dart
+++ lib/data/models/cinema.dart
@@ -1,20 +1,14 @@
 class Cinema {
   final int id;
   final String name;
-  final int cityId; // 🟢 1. TAMBAHKAN PROPERTI RELASI KOTA
 
-  Cinema({
-    required this.id, 
-    required this.name,
-    required this.cityId, // 🟢 2. MASUKKAN KE CONSTRUCTOR
-  });
+  Cinema({required this.id, required this.name});
 
   factory Cinema.fromJson(Map<String, dynamic> json) {
     return Cinema(
       id: json['id'] ?? 0,
+      // Antisipasi nama key dari Golang (bisa 'name' atau 'cinema_name')
       name: json['name'] ?? json['cinema_name'] ?? 'Bioskop', 
-      // 🟢 3. AMBIL DATA CITY_ID DARI JSON BACKEND GOLANG
-      cityId: json['city_id'] ?? json['cityId'] ?? 0, 
     );
   }
 }
